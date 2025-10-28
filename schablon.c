b5084fec356bf4fc6cd90b38c751e70d07056b37
hexen <koydano@gmail.com>
2025-10-28 10:22:10 +0100

    Тестирую сохранения в ветках.

--- schablon.c
+++ schablon.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <windows.h> // Für system("chcp 1252") und system("cls")
-#pragma execution_character_set("utf-8")
+#include <windows.h> // Für system("chcp 1252") und system("cls") помогает отображать корректно символы в терминале VsCode.
+#pragma execution_character_set("utf-8")// Für system("chcp 1252") und system("cls") помогает отображать корректно символы в терминале VsCode.
 
 int main() {
-	system("chcp 65001"); // Setzt die Codepage auf 65001 (UTF-8) für die korrekte Anzeige von Umlauten
-	system("cls"); // Löscht die Konsole
+	system("chcp 65001"); // Setzt die Codepage auf 65001 (UTF-8) für die korrekte Anzeige von Umlauten // Für system("chcp 1252") und system("cls") помогает отображать корректно символы в терминале VsCode.
+	system("cls"); // Löscht die Konsole// Für system("chcp 1252") und system("cls") помогает отображать корректно символы в терминале VsCode.
 
 
 	return 0;
