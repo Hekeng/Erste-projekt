a858265413e0efd1651aa3de01874916893ef0b2
hexen <koydano@gmail.com>
2025-11-20 14:35:17 +0100

    +1

--- 05_05_03.c
+++ 05_05_03.c
@@ -9,6 +9,7 @@ typedef struct {
     int laenge;
 } WORT;
 
+void inputwort(struct WORT *ptr);
 
 int main() {
 	system("chcp 65001"); // Setzt die Codepage auf 65001 (UTF-8) für die korrekte Anzeige von Umlauten // Für system("chcp 1252") und system("cls") помогает отображать корректно символы в терминале VsCode.
@@ -20,9 +21,14 @@ int main() {
 	return 0;
 } 
 
-void inputwort (void){
-	
-	printf("Geben Sie einen ersten Satz ein (max 100 Zeichen):\n");
-	scanf(" %100[^\n]", s1);
-	while (getchar() != '\n');
+void inputwort(struct WORT *ptr) { 
+
+    printf("Geben Sie ein Wort ein (max 99 Zeichen):\n");
+
+    // 💡 1. Используем ptr->text для обращения к полю структуры.
+    // 💡 2. Убран лишний пробел перед % для корректного считывания строки.
+    scanf("%50[^\n]", ptr->text); 
+
+    // 💡 3. Очистка буфера, предотвращающая проблемы при следующем вводе.
+    while (getchar() != '\n'); 
 }
